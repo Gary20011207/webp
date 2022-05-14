014b18fa58d4b272d1b89a02018799dfbe6aca5a
陳冠宇 <100269264+Gary20011207@users.noreply.github.com>
2022-05-14 23:51:33 +0800

    Add files via upload

---
+++ qz8/src/cgu_hello.js
@@ -0,0 +1,8 @@
+const styleArgument = {fontSize: '100px', color:'red'};
+
+const HelloCGU=()=>{
+    var output=[];
+    output.push(<h1 style = {styleArgument} >Hello CGU!</h1>)
+    return output;
+  }
+export default HelloCGU;
