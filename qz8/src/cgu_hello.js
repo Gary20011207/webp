357afc46e84d60eb754de03157299c7012694fa0
陳冠宇 <100269264+Gary20011207@users.noreply.github.com>
2022-05-14 23:52:40 +0800

    Delete qz8 directory

--- qz8/src/cgu_hello.js
+++
@@ -1,8 +0,0 @@
-const styleArgument = {fontSize: '100px', color:'red'};
-
-const HelloCGU=()=>{
-    var output=[];
-    output.push(<h1 style = {styleArgument} >Hello CGU!</h1>)
-    return output;
-  }
-export default HelloCGU;
